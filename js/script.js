f95e57b98fa1373dbbff7dc59497359964a29e94
Mateusz Gomolka <mdm.gomolka@gmail.com>
2022-08-03 00:51:30 +0100

    Change script.js Add computer moves

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-let computerMove = 'rock'
-let playerMove = 'paper'
+let randomNumber = Math.floor(Math.random() * 3 + 1)
 
-printMessage('I played ' + computerMove + '! If your move is a ' + playerMove + ', then you win!')
+console.log('Drawn number is: ' + randomNumber)
+
+let computerMove = 'unknown move'
+
+if (randomNumber == 1) {
+	computerMove = 'rock'
+} else if (randomNumber == 2) {
+	computerMove = 'paper'
+} else if (randomNumber == 3) {
+	computerMove = 'scissor'
+}
+
+printMessage('My move is ' + computerMove)
