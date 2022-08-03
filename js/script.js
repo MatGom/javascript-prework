e3cc518c52ba58cbaa7e584318f399d28bea5341
Mateusz Gomolka <mdm.gomolka@gmail.com>
2022-08-03 20:02:56 +0100

    Create functions for conditional statements

--- js/script.js
+++ js/script.js
@@ -1,43 +1,37 @@
-let randomNumber = Math.floor(Math.random() * 3 + 1)
-
-console.log('Drawn number is: ' + randomNumber)
-
-let computerMove = 'unknown move'
-
-if (randomNumber == 1) {
-	computerMove = 'rock'
-} else if (randomNumber == 2) {
-	computerMove = 'paper'
-} else if (randomNumber == 3) {
-	computerMove = 'scissor'
-}
-
-printMessage('My move is ' + computerMove)
-
 let playerInput = prompt('Make your move! 1: rock, 2: paper, 3: scissors.')
+let randomNumber = Math.floor(Math.random() * 3 + 1)
+let computerMove = getMoveName(randomNumber)
+let playerMove = getMoveName(playerInput)
 
-console.log('Player chose: ' + playerInput)
+function getMoveName(argMoveId) {
+	if (argMoveId == 1) {
+		return 'rock'
+	} else if (argMoveId == 2) {
+		return 'paper'
+	} else if (argMoveId == 3) {
+		return 'scissors'
+	}
 
-let playerMove = 'unknown move'
-
-if (playerInput == '1') {
-	playerMove = 'rock'
-} else if (playerInput == '2') {
-	playerMove = 'paper'
-} else if (playerInput == '3') {
-	playerMove = 'scissors'
+	printMessage("I don't know a move of id " + argMoveId + '.')
+	return 'unknown move'
 }
 
-printMessage('Your move is: ' + playerMove)
+function displayResult(argComputerMove, argPlayerMove) {
+	printMessage('My move is ' + computerMove)
+	printMessage('Your move is: ' + playerMove)
 
-if (
-	(computerMove == 'rock' && playerMove == 'paper') ||
-	(computerMove == 'paper' && playerMove == 'scissors') ||
-	(computerMove == 'scissors' && playerMove == 'rock')
-) {
-	printMessage('You win!')
-} else if (computerMove == playerMove) {
-	printMessage('Draw!')
-} else {
-	printMessage('You lose!')
+	if (
+		(argComputerMove == 'rock' && argPlayerMove == 'paper') ||
+		(argComputerMove == 'paper' && argPlayerMove == 'scissors') ||
+		(argComputerMove == 'scissors' && argPlayerMove == 'rock')
+	) {
+		printMessage('You win!')
+	} else if (argComputerMove == argPlayerMove) {
+		printMessage('Draw!')
+	} else {
+		printMessage('You lose!')
+	}
+	console.log('moves:', argComputerMove, argPlayerMove)
 }
+
+displayResult(computerMove, playerMove)
