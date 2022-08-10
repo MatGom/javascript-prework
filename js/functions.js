0e95e2c172a4db0e8e165baa0963dd19fecdee4e
Mateusz Gomolka <mdm.gomolka@gmail.com>
2022-08-10 18:21:48 +0100

    Add functions to variables Move code out of global scope

--- js/functions.js
+++ js/functions.js
@@ -1,26 +1,12 @@
-function printMessage(msg) {
+const printMessage = function (msg) {
 	let div = document.createElement('div')
 	div.innerHTML = msg
 	document.getElementById('messages').appendChild(div)
 }
 
-function clearMessages() {
+const clearMessages = function () {
 	document.getElementById('messages').innerHTML = ''
 	document.querySelector('h3').style.display = 'block'
-}
-
-const resetStats = () => {
-	document.querySelector('.player-wins').textContent = 0
-	document.querySelector('.computer-wins').textContent = 0
-	document.querySelector('.player-rate').textContent = `0%`
-	document.querySelector('.computer-rate').textContent = `0%`
-	document.querySelector('.games-played').textContent = 0
-	playerWins = 0
-	computerWins = 0
-	gamesPlayed = 0
-	playerWinrate = 0
-	computerWinrate = 0
-	clearMessages()
 }
 
 const showPopup = () => {
@@ -31,13 +17,4 @@ const showPopup = () => {
 const closePopup = () => {
 	document.querySelector('.info-popup').classList.add('hidden')
 	document.querySelector('.popup-shadow').classList.add('hidden')
-}
-
-const calculateWinrate = () => {
-	document.querySelector('.player-wins').textContent = playerWins
-	document.querySelector('.computer-wins').textContent = computerWins
-	playerWinrate = ((playerWins / gamesPlayed) * 100).toFixed(1)
-	document.querySelector('.player-rate').textContent = `${playerWinrate}%`
-	computerWinrate = ((computerWins / gamesPlayed) * 100).toFixed(1)
-	document.querySelector('.computer-rate').textContent = `${computerWinrate}%`
 }
--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
-let playerWins = 0
-let computerWins = 0
-let gamesPlayed = 0
-let playerWinrate = 0
-let computerWinrate = 0
-let rock = '<img src="images/rock_sci_right.png">'
-let paper = '<img src="images/paper_rock_right.png">'
-let scissors = '<img src="images/sci_paper_right.png">'
+{
+	let playerWins = 0
+	let computerWins = 0
+	let gamesPlayed = 0
+	let playerWinrate = 0
+	let computerWinrate = 0
+	const rock = '<img src="images/rock_sci_right.png">'
+	const paper = '<img src="images/paper_rock_right.png">'
+	const scissors = '<img src="images/sci_paper_right.png">'
 
-function playGame(playerInput) {
-	clearMessages()
-
-	gamesPlayed++
-	document.querySelector('.games-played').textContent = gamesPlayed
-
-	let randomNumber = Math.floor(Math.random() * 3 + 1)
-	let computerMove = getMoveName(randomNumber)
-	let playerMove = getMoveName(playerInput)
-
-	function getMoveName(argMoveId) {
+	const getMoveName = function (argMoveId) {
 		if (argMoveId == 1) {
 			return rock
 		} else if (argMoveId == 2) {
@@ -27,77 +18,87 @@ function playGame(playerInput) {
 		}
 	}
 
-	function displayResult(argComputerMove, argPlayerMove) {
-		printMessage('Your move is')
-		printMessage('My move is')
-		printMessage(playerMove)
-		printMessage(computerMove)
-
-		if (
-			(argComputerMove == rock && argPlayerMove == paper) ||
-			(argComputerMove == paper && argPlayerMove == scissors) ||
-			(argComputerMove == scissors && argPlayerMove == rock)
-		) {
-			printMessage('You win!')
-			playerWins++
-			calculateWinrate()
-		} else if (argComputerMove == argPlayerMove) {
-			printMessage('Draw!')
-			calculateWinrate()
-		} else {
-			printMessage('You lose!')
-			computerWins++
-			calculateWinrate()
-		}
+	const resetStats = () => {
+		document.querySelector('.player-wins').textContent = 0
+		document.querySelector('.computer-wins').textContent = 0
+		document.querySelector('.player-rate').textContent = `0%`
+		document.querySelector('.computer-rate').textContent = `0%`
+		document.querySelector('.games-played').textContent = 0
+		playerWins = 0
+		computerWins = 0
+		gamesPlayed = 0
+		playerWinrate = 0
+		computerWinrate = 0
+		clearMessages()
 	}
 
-	document.querySelectorAll('button').forEach(button => {
-		button.disabled = true
-		setTimeout(() => {
-			button.disabled = false
-		}, 1100)
+	const calculateWinrate = () => {
+		document.querySelector('.player-wins').textContent = playerWins
+		document.querySelector('.computer-wins').textContent = computerWins
+		playerWinrate = ((playerWins / gamesPlayed) * 100).toFixed(1)
+		document.querySelector('.player-rate').textContent = `${playerWinrate}%`
+		computerWinrate = ((computerWins / gamesPlayed) * 100).toFixed(1)
+		document.querySelector('.computer-rate').textContent = `${computerWinrate}%`
+	}
+
+	const playGame = function (playerInput) {
+		clearMessages()
+
+		gamesPlayed++
+		document.querySelector('.games-played').textContent = gamesPlayed
+
+		let randomNumber = Math.floor(Math.random() * 3 + 1)
+		let computerMove = getMoveName(randomNumber)
+		let playerMove = getMoveName(playerInput)
+
+		getMoveName()
+
+		const displayResult = function (argComputerMove, argPlayerMove) {
+			printMessage('Your move is')
+			printMessage('My move is')
+			printMessage(playerMove)
+			printMessage(computerMove)
+
+			if (
+				(argComputerMove == rock && argPlayerMove == paper) ||
+				(argComputerMove == paper && argPlayerMove == scissors) ||
+				(argComputerMove == scissors && argPlayerMove == rock)
+			) {
+				printMessage('You win!')
+				playerWins++
+				calculateWinrate()
+			} else if (argComputerMove == argPlayerMove) {
+				printMessage('Draw!')
+				calculateWinrate()
+			} else {
+				printMessage('You lose!')
+				computerWins++
+				calculateWinrate()
+			}
+		}
+
+		document.querySelectorAll('button').forEach(button => {
+			button.disabled = true
+			setTimeout(() => {
+				button.disabled = false
+			}, 1100)
+		})
+
+		document.querySelector('h3').style.display = 'none'
+
+		displayResult(computerMove, playerMove)
+	}
+
+	document.getElementById('play-rock').addEventListener('click', function () {
+		playGame(1)
 	})
-
-	document.querySelector('h3').style.display = 'none'
-
-	displayResult(computerMove, playerMove)
+	document.getElementById('play-paper').addEventListener('click', function () {
+		playGame(2)
+	})
+	document.getElementById('play-scissors').addEventListener('click', function () {
+		playGame(3)
+	})
+	document.querySelector('.reset-btn').addEventListener('click', resetStats)
+	document.querySelector('.info').addEventListener('click', showPopup)
+	document.querySelector('.close-info').addEventListener('click', closePopup)
 }
-
-document.getElementById('play-rock').addEventListener('click', function () {
-	playGame(1)
-})
-document.getElementById('play-paper').addEventListener('click', function () {
-	playGame(2)
-})
-document.getElementById('play-scissors').addEventListener('click', function () {
-	playGame(3)
-})
-document.querySelector('.reset-btn').addEventListener('click', resetStats)
-document.querySelector('.info').addEventListener('click', showPopup)
-document.querySelector('.close-info').addEventListener('click', closePopup)
-
-// if (computerMove == rock && playerMove == paper) {
-// 	rock = '<img src="images/rock_paper_left.png">'
-// 	paper = '<img src="images/paper_rock_right.png">'
-// } else if (computerMove == paper && playerMove == scissors) {
-// 	paper = '<img src="images/paper_sci_left.png">'
-// 	scissors = '<img src="images/sci_paper_right.png">'
-// } else if (computerMove == scissors && playerMove == rock) {
-// 	scissors = '<img src="images/sci_rock_left.png">'
-// 	rock = '<img src="images/rock_sci_right.png">'
-// } else if (computerMove == rock && playerMove == rock) {
-// 	rock = '<img src="images/rock_left_2.png">'
-// } else if (computerMove == paper && playerMove == paper) {
-// 	paper = '<img src="images/paper_left_2.png">'
-// } else if (computerMove == scissors && playerMove == scissors) {
-// 	scissors = '<img src="images/sci_left_2.png">'
-// } else if (computerMove == paper && playerMove == rock) {
-// 	rock = '<img src="images/rock_paper_right.png">'
-// 	paper = '<img src="images/paper_rock_left.png">'
-// } else if (computerMove == scissors && playerMove == paper) {
-// 	paper = '<img src="images/paper_sci_right.png">'
-// 	scissors = '<img src="images/sci_paper_left.png">'
-// } else if (computerMove == rock && playerMove == scissors) {
-// 	scissors = '<img src="images/sci_rock_right.png">'
-// 	rock = '<img src="images/rock_sci_left.png">'
-// }
