7387318cadf886112e154397ceff0fb89ef6aebc
Mateusz Gomolka <mdm.gomolka@gmail.com>
2022-08-11 19:41:40 +0100

    Move js variables to their scopes

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,28 @@
+const introTitle = document.querySelector('h3');
+const messagesBox = document.getElementById('messages');
+
+const showPopupIcon = document.querySelector('.info');
+const closePopupIcon = document.querySelector('.close-info');
+const infoPopup = document.querySelector('.info-popup');
+const infoPopupShadow = document.querySelector('.popup-shadow');
+
 const printMessage = function (msg) {
-	let div = document.createElement('div')
-	div.innerHTML = msg
-	messagesBox.appendChild(div)
-}
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	messagesBox.appendChild(div);
+};
 
 const clearMessages = function () {
-	messagesBox.innerHTML = ''
-	introTitle.style.display = 'block'
-}
+	messagesBox.innerHTML = '';
+	introTitle.style.display = 'block';
+};
 
 const showPopup = () => {
-	infoPopup.classList.remove('hidden')
-	infoPopupShadow.classList.remove('hidden')
-}
+	infoPopup.classList.remove('hidden');
+	infoPopupShadow.classList.remove('hidden');
+};
 
 const closePopup = () => {
-	infoPopup.classList.add('hidden')
-	infoPopupShadow.classList.add('hidden')
-}
+	infoPopup.classList.add('hidden');
+	infoPopupShadow.classList.add('hidden');
+};
--- js/script.js
+++ js/script.js
@@ -1,104 +1,117 @@
 {
-	let playerWins = 0
-	let computerWins = 0
-	let gamesPlayed = 0
-	let playerWinrate = 0
-	let computerWinrate = 0
-	const rock = '<img src="images/rock_sci_right.png">'
-	const paper = '<img src="images/paper_rock_right.png">'
-	const scissors = '<img src="images/sci_paper_right.png">'
+	let playerWins = 0;
+	let computerWins = 0;
+	let gamesPlayed = 0;
+	let playerWinrate = 0;
+	let computerWinrate = 0;
+
+	const rock = '<img src="images/rock_sci_right.png">';
+	const paper = '<img src="images/paper_rock_right.png">';
+	const scissors = '<img src="images/sci_paper_right.png">';
+
+	const rockBtn = document.getElementById('play-rock');
+	const paperBtn = document.getElementById('play-paper');
+	const scissorsBtn = document.getElementById('play-scissors');
+	const resetBtn = document.querySelector('.reset-btn');
+	const playBtns = document.querySelectorAll('button');
+
+	const playerWinsCounter = document.querySelector('.player-wins');
+	const computerWinsCounter = document.querySelector('.computer-wins');
+	const playerWinrateStat = document.querySelector('.player-rate');
+	const computerWinrateStat = document.querySelector('.computer-rate');
+	const gamesCounter = document.querySelector('.games-played');
 
 	const getMoveName = function (argMoveId) {
 		if (argMoveId == 1) {
-			return rock
+			return rock;
 		} else if (argMoveId == 2) {
-			return paper
+			return paper;
 		} else if (argMoveId == 3) {
-			return scissors
+			return scissors;
 		}
-	}
+	};
 
 	const resetStats = () => {
-		playerWinsCounter.textContent = 0
-		computerWinsCounter.textContent = 0
-		playerWinrateStat.textContent = `0%`
-		computerWinrateStat.textContent = `0%`
-		gamesCounter.textContent = 0
-		playerWins = 0
-		computerWins = 0
-		gamesPlayed = 0
-		playerWinrate = 0
-		computerWinrate = 0
-		clearMessages()
-	}
+		playerWinsCounter.textContent = 0;
+		computerWinsCounter.textContent = 0;
+		playerWinrateStat.textContent = `0%`;
+		computerWinrateStat.textContent = `0%`;
+		gamesCounter.textContent = 0;
+		playerWins = 0;
+		computerWins = 0;
+		gamesPlayed = 0;
+		playerWinrate = 0;
+		computerWinrate = 0;
+		clearMessages();
+	};
 
 	const calculateWinrate = () => {
-		playerWinsCounter.textContent = playerWins
-		computerWinsCounter.textContent = computerWins
-		playerWinrate = ((playerWins / gamesPlayed) * 100).toFixed(1)
-		playerWinrateStat.textContent = `${playerWinrate}%`
-		computerWinrate = ((computerWins / gamesPlayed) * 100).toFixed(1)
-		computerWinrateStat.textContent = `${computerWinrate}%`
-	}
+		playerWinsCounter.textContent = playerWins;
+		computerWinsCounter.textContent = computerWins;
+		playerWinrate = ((playerWins / gamesPlayed) * 100).toFixed(1);
+		playerWinrateStat.textContent = `${playerWinrate}%`;
+		computerWinrate = ((computerWins / gamesPlayed) * 100).toFixed(1);
+		computerWinrateStat.textContent = `${computerWinrate}%`;
+	};
 
 	const playGame = function (playerInput) {
-		clearMessages()
+		clearMessages();
 
-		gamesPlayed++
-		gamesCounter.textContent = gamesPlayed
+		gamesPlayed++;
+		gamesCounter.textContent = gamesPlayed;
 
-		let randomNumber = Math.floor(Math.random() * 3 + 1)
-		let computerMove = getMoveName(randomNumber)
-		let playerMove = getMoveName(playerInput)
+		let randomNumber = Math.floor(Math.random() * 3 + 1);
+		let computerMove = getMoveName(randomNumber);
+		let playerMove = getMoveName(playerInput);
 
-		getMoveName()
+		getMoveName();
 
 		const displayResult = function (argComputerMove, argPlayerMove) {
-			printMessage('Your move is')
-			printMessage('My move is')
-			printMessage(playerMove)
-			printMessage(computerMove)
+			printMessage('Your move is');
+			printMessage('My move is');
+			printMessage(playerMove);
+			printMessage(computerMove);
 
 			if (
 				(argComputerMove == rock && argPlayerMove == paper) ||
 				(argComputerMove == paper && argPlayerMove == scissors) ||
 				(argComputerMove == scissors && argPlayerMove == rock)
 			) {
-				printMessage('You win!')
-				playerWins++
-				calculateWinrate()
+				printMessage('You win!');
+				playerWins++;
+				calculateWinrate();
 			} else if (argComputerMove == argPlayerMove) {
-				printMessage('Draw!')
-				calculateWinrate()
+				printMessage('Draw!');
+				calculateWinrate();
 			} else {
-				printMessage('You lose!')
-				computerWins++
-				calculateWinrate()
+				printMessage('You lose!');
+				computerWins++;
+				calculateWinrate();
 			}
-		}
+		};
 
 		playBtns.forEach(button => {
-			button.disabled = true
+			button.disabled = true;
 			setTimeout(() => {
-				button.disabled = false
-			}, 1100)
-		})
+				button.disabled = false;
+			}, 1100);
+		});
 
-		introTitle.style.display = 'none'
+		introTitle.style.display = 'none';
 
-		displayResult(computerMove, playerMove)
-	}
+		displayResult(computerMove, playerMove);
+	};
 
 	rockBtn.addEventListener('click', function () {
-		playGame(1)
-	})
+		playGame(1);
+	});
 	paperBtn.addEventListener('click', function () {
-		playGame(2)
-	})
+		playGame(2);
+	});
 	scissorsBtn.addEventListener('click', function () {
-		playGame(3)
-	})
-	resetBtn.addEventListener('click', resetStats)
-	showPopupIcon.addEventListener('click', showPopup)
-	closePopupIcon.addEventListener('click', closePopup)
+		playGame(3);
+	});
+	resetBtn.addEventListener('click', resetStats);
+	showPopupIcon.addEventListener('click', showPopup);
+	closePopupIcon.addEventListener('click', closePopup);
 }
